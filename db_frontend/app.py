da45b0f469b21d65d11209581092839b2ed2680d
necoder27 <95191364+necoder27@users.noreply.github.com>
2024-01-02 18:26:37 +0100

    finish frontend with different view selection and add, update and delete

--- db_frontend/app.py
+++ db_frontend/app.py
@@ -1,8 +1,159 @@
-from flask import Flask, render_template
+from bson import ObjectId
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('localhost', 27017, username='root', password='1234')
+db = client['KeanuBeats']
+artists = db['artists']
+albums = db['albums']
+songs = db['songs']
+playlists = db['playlists']
+
+
+def view_query():
+    pipeline = [
+        {
+            "$lookup": {
+                "from": "songs",
+                "localField": "songs",
+                "foreignField": "_id",
+                "as": "songs_info"
+            }
+        },
+        {
+            "$unwind": "$songs_info"
+        },
+        {
+            "$lookup": {
+                "from": "artists",
+                "localField": "songs_info.artist_id",
+                "foreignField": "_id",
+                "as": "artist_info"
+            }
+        },
+        {
+            "$unwind": "$artist_info"
+        },
+        {
+            "$lookup": {
+                "from": "albums",
+                "localField": "songs_info.album_id",
+                "foreignField": "_id",
+                "as": "album_info"
+            }
+        },
+        {
+            "$unwind": "$album_info"
+        },
+        {
+            "$project": {
+                "playlists_id": "$_id",
+                "playlists_name": "$name",
+                "songs_id": "$songs_info._id",
+                "songs_title": "$songs_info.title",
+                "songs_length": "$songs_info.length",
+                "songs_rating": "$songs_info.rating",
+                "songs_yt_link": "$songs_info.yt_link",
+                "artists_id": "$artist_info._id",
+                "artists_name": "$artist_info.name",
+                "albums_id": "$album_info._id",
+                "albums_name": "$album_info.name"
+            }
+        }
+    ]
+    result = list(playlists.aggregate(pipeline))
+    return result
+
+
+def add_row_numbers(result):
+    for i, row in enumerate(result):
+        row['row_number'] = i + 1
+    return result
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    query_result = view_query()
+    artists_result = list(artists.find())
+    albums_result = list(albums.find())
+    songs_result = list(songs.find())
+    playlists_result = list(playlists.find())
+
+    query_result = add_row_numbers(query_result)
+    artists_result = add_row_numbers(artists_result)
+    albums_result = add_row_numbers(albums_result)
+    songs_result = add_row_numbers(songs_result)
+    playlists_result = add_row_numbers(playlists_result)
+
+    return render_template('index.html', query_result=query_result, artists_result=artists_result, albums_result=albums_result, songs_result=songs_result, playlists_result=playlists_result)
+
+
+@app.route('/add_document', methods=['POST'])
+def add_document():
+    collection_name = request.form['collection_name']
+
+    if collection_name not in ['artists', 'albums', 'songs', 'playlists']:
+        return redirect(url_for('index'))
+
+    new_document = {}
+
+    if collection_name == 'artists':
+        new_document['name'] = request.form['artist_name']
+    elif collection_name == 'albums':
+        new_document['name'] = request.form['album_name']
+    elif collection_name == 'songs':
+        new_document['title'] = request.form['song_title']
+        new_document['length'] = request.form['song_length']
+        new_document['rating'] = request.form['song_rating']
+        new_document['yt_link'] = request.form['song_yt_link']
+        new_document['artist_id'] = request.form['song_artist_id']
+        new_document['album_id'] = request.form['song_album_id']
+    elif collection_name == 'playlists':
+        new_document['name'] = request.form['playlist_name']
+
+    db[collection_name].insert_one(new_document)
+
+    return redirect(url_for('index'))
+
+
+@app.route('/delete_document', methods=['POST'])
+def delete_document():
+    document_id = request.form['document_id']
+    collection_name = request.form['collection_name']
+
+    if collection_name not in ['artists', 'albums', 'songs', 'playlists']:
+        return redirect(url_for('index'))
+
+    db[collection_name].delete_one({'_id': ObjectId(document_id)})
+
+    return redirect(url_for('index'))
+
+
+@app.route('/update_document', methods=['POST'])
+def update_document():
+    document_id = ObjectId(request.form['document_id'])
+    collection_name = request.form['collection_name']
+
+    document = db[collection_name].find_one({'_id': document_id})
+
+    if not document:
+        return "Error: Document does not exist", 400
+
+    if collection_name not in ['artists', 'albums', 'songs', 'playlists']:
+        return redirect(url_for('index'))
+
+    updates = {}
+    for key, value in request.form.items():
+        if key != 'document_id' and key != 'collection_name' and value:
+            updates['name'] = value
+
+    print(document_id)
+    print(updates)
+
+    result = db[collection_name].update_one({'_id': document_id}, {'$set': updates})
+
+    if result.matched_count == 0:
+        return "Error: No document found with given id", 400
+
+    return redirect(url_for('index'))
--- db_frontend/mongo_connection.py
+++ db_frontend/mongo_connection.py
@@ -1,8 +1,22 @@
+from bson import Decimal128
 from faker import Faker
 from pymongo import MongoClient
 faker: Faker = Faker()
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-client = MongoClient('localhost', 27017, username='root', password='1234')
+uri = "mongodb+srv://root:1234@dbivergleichsprojekt.ipzeaff.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# client = MongoClient('localhost', 27017, username='root', password='1234')
+# client = MongoClient('mongodb+srv://root:1234@dbimusicplayer.b6ocnvr.mongodb.net/?retryWrites=true&w=majority')
 db = client['KeanuBeats']
 
 db.drop_collection('artists')
@@ -27,3 +41,29 @@ for i in range(100):
     albums.insert_one(album)
 
     song = {
+        "title": faker.sentence(),
+        "length": Decimal128(faker.pydecimal(left_digits=2, right_digits=2, positive=True)),
+        "rating": Decimal128(faker.pydecimal(left_digits=1, right_digits=1, positive=True)),
+        "yt_link": faker.url(),
+        "artist_id": artists.aggregate([{"$sample": {"size": 1}}]).next()['_id'],
+        "album_id": albums.aggregate([{"$sample": {"size": 1}}]).next()['_id']
+    }
+    songs.insert_one(song)
+
+    playlist = {
+        "name": faker.catch_phrase(),
+        "songs": [songs.aggregate([{"$sample": {"size": 1}}]).next()['_id'] for _ in range(5)] # check if what this doing is correct: new ObjectId()?????
+    }
+    playlists.insert_one(playlist)
+
+    albumHasArtist = {
+        "artist_id": artists.aggregate([{"$sample": {"size": 1}}]).next()['_id'],
+        "album_id": albums.aggregate([{"$sample": {"size": 1}}]).next()['_id']
+    }
+    albumsHaveArtists.insert_one(albumHasArtist)
+
+    playlistHasSong = {
+        "song_id": songs.aggregate([{"$sample": {"size": 1}}]).next()['_id'],
+        "playlist_id": playlists.aggregate([{"$sample": {"size": 1}}]).next()['_id']
+    }
+    playlistsHaveSongs.insert_one(playlistHasSong)
